9ed465f02c8b07bf5cdcf0885afb56ec4b995721
Carolan <carolana@egb076000132.lancs.local>
2018-02-07 11:44:04 +0000

    Fixed timeout errors with new reciving socket

--- Traceroute.py
+++ Traceroute.py
@@ -10,8 +10,8 @@ import binascii
 ICMP_ECHO_REQUEST = 8 #ICMP type code for echo request messages
 ICMP_ECHO_REPLY = 0 #ICMP type code for echo reply messages
 
-HOP_NUMBER = 0
-CURR_IP = "NULL"
+HOP_NUMBER = 0 # Current hop number
+CURR_IP = "NULL" # IP adress of responder
 
 
 def checksum(string): 
@@ -46,10 +46,10 @@ def receiveOnePing(icmpSocket, destinationAddress, timeSent):
 	global HOP_NUMBER
 	global CURR_IP
 	
-	# 1. Wait for the socket to receive a reply
-	# 2. Once received, record time of receipt, otherwise, handle a timeout
+	# Wait for the socket to receive a reply
+	# Once received, record time of receipt, otherwise, handle a timeout
 	try:
-		packet_data, address = icmpSocket.recv(1024)
+		packet_data, address = icmpSocket.recvfrom(1024)
 		recived = time.time()
 	except:
 		print("Time Out")
@@ -59,11 +59,11 @@ def receiveOnePing(icmpSocket, destinationAddress, timeSent):
 	address = address[0]
 	CURR_IP = address
 	try:
-		addressName = gethostbyaddr(address)
+		addressName, x, y = socket.gethostbyaddr(address)
 	except:
 		addressName = "NULL"
 	
-	# 3. Compare the time of receipt to time of sending, producing the total network delay
+	# Compare the time of receipt to time of sending, producing the total network delay
 	delay = recived - timeSent
 	delay = round(delay * 1000.0, 3)
 	
@@ -71,34 +71,36 @@ def receiveOnePing(icmpSocket, destinationAddress, timeSent):
 	print("Hop " + str(HOP_NUMBER) + " Delay: " + str(delay) + " ms, IP Address: " + address + ", Host Name: " + addressName)
 	
 def sendOnePing(icmpSocket, destinationAddress):
-	# 1. Build ICMP header
+	# Build the ICMP header
 	header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, ICMP_ECHO_REPLY, 0, 1, 1)
-	# 2. Checksum ICMP packet using given function
+	# Checksum ICMP packet using given function
 	chkSum = checksum(header)
-	# 3. Insert checksum into packet
+	# Insert checksum into packet
 	packet = struct.pack("bbHHh", ICMP_ECHO_REQUEST, ICMP_ECHO_REPLY, chkSum, 1, 1)
-	# 4. Send packet using socket
+	# Send packet using socket
 	icmpSocket.send(packet)
-	# 5. Record time of sending
+	# Record time of sending
 	timeSent = time.time()
 	return timeSent
 	
 
 def doOnePing(destinationAddress, timeout): 
 	global HOP_NUMBER
-	# 1. Create ICMP socket
+	# Create ICMP socket for send and recive
 	icmp = socket.getprotobyname("icmp")
 	sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-	#sock.setsockopt(socket.SOL_IP, socket.IP_TTL, HOP_NUMBER)
+	recv_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
 	sock.connect((destinationAddress, 80))
-	sock.settimeout(timeout)
-	# 2. Call sendOnePing function
+	recv_sock.settimeout(timeout)
+	sock.setsockopt(socket.SOL_IP, socket.IP_TTL, HOP_NUMBER)
+	# Call sendOnePing function
 	timeSent = sendOnePing(sock, destinationAddress)
-	# 3. Call receiveOnePing function
-	receiveOnePing(sock, destinationAddress, timeSent)
-	# 4. Close ICMP socket
+	# Call receiveOnePing function
+	receiveOnePing(recv_sock, destinationAddress, timeSent)
+	# Close ICMP sockets
 	sock.close()
-	# 5. Return total network delay
+	recv_sock.close()
+	
 	
 def ping(host):
 	global HOP_NUMBER
@@ -106,17 +108,16 @@ def ping(host):
 	
 	timeout = int(input("Input the timeout value in seconds: "))
 	
-	# 1. Look up hostname, resolving it to an IP address
+	# look up hostname, resolving it to an IP address
 	ipAddress = socket.gethostbyname(host)
-	# 2. Call doOnePing function
+	# Call doOnePing function
 	while(CURR_IP != ipAddress):
 		HOP_NUMBER = HOP_NUMBER + 1
 		doOnePing(ipAddress, timeout)
 		time.sleep(1)
-	# 3. Print out the returned delay
-	# 4. Continue this process until stopped	
+	# Continue this process until end is reached	
 	
-ping("www.lancaster.ac.uk")
+ping("www.bbc.co.uk")
 
 
 
